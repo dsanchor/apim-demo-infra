70e48695f56a0d5edd72b227c7e72f232d330570
dsanchor <sancheski@gmail.com>
2023-03-09 11:42:25 +0100

    Added starter and premium products

--- apim/main.tf
+++ apim/main.tf
@@ -46,6 +46,76 @@ resource "azurerm_api_management" "apim" {
   sku_name = local.sku_name
 }
 
+resource "azurerm_api_management_product" "starter" {
+  product_id            = "starter"
+  api_management_name   = azurerm_api_management.apim.name
+  resource_group_name   = azurerm_resource_group.rg.name
+  display_name          = "Starter"
+  subscription_required = true
+  approval_required     = false
+  published             = true
+}
+
+resource "azurerm_api_management_product_policy" "starter" {
+  product_id          = data.azurerm_api_management_product.starter.product_id
+  api_management_name = data.azurerm_api_management_product.apim.api_management_name
+  resource_group_name = data.azurerm_api_management_product.rg.resource_group_name
+
+  xml_content = <<XML
+  <policies>
+    <inbound>
+      <base />
+      <rate-limit calls="10" renewal-period="60" counter-key="@(context.Subscription.Id)">
+        <renewal-key>
+          <header name="Ocp-Apim-Subscription-Key" exists-action="override" />
+        </renewal-key>
+      </rate-limit>
+      <quota calls="100" renewal-period="3600" counter-key="@(context.Subscription.Id)">
+        <renewal-key>
+          <header name="Ocp-Apim-Subscription-Key" exists-action="override" />
+        </renewal-key>
+      </quota>
+    </inbound>
+  </policies>
+  XML
+
+}
+
+resource "azurerm_api_management_product" "premium" {
+  product_id            = "premium"
+  api_management_name   = azurerm_api_management.apim.name
+  resource_group_name   = azurerm_resource_group.rg.name
+  display_name          = "Premium"
+  subscription_required = true
+  approval_required     = true
+  published             = true
+}
+
+resource "azurerm_api_management_product_policy" "premium" {
+  product_id          = data.azurerm_api_management_product.premium.product_id
+  api_management_name = data.azurerm_api_management_product.apim.api_management_name
+  resource_group_name = data.azurerm_api_management_product.rg.resource_group_name
+
+  xml_content = <<XML
+  <policies>
+    <inbound>
+      <base />
+      <rate-limit calls="600" renewal-period="60" counter-key="@(context.Subscription.Id)">
+        <renewal-key>
+          <header name="Ocp-Apim-Subscription-Key" exists-action="override" />
+        </renewal-key>
+      </rate-limit>
+      <quota calls="10000" renewal-period="3600" counter-key="@(context.Subscription.Id)">
+        <renewal-key>
+          <header name="Ocp-Apim-Subscription-Key" exists-action="override" />
+        </renewal-key>
+      </quota>
+    </inbound>
+  </policies>
+  XML
+
+}
+
 # Create Storage Account
 resource "azurerm_storage_account" "sa" {
   name                      = local.storageAccountName
