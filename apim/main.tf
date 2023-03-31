f754507b15f22208d1f0404e2213e9a418464415
heblasco <heblasco@microsot.com>
2023-03-31 14:35:44 +0200

    commit

--- apim/main.tf
+++ apim/main.tf
@@ -127,8 +127,8 @@ resource "azurerm_api_management_user" "jd" {
 resource "azurerm_api_management_subscription" "jsstarter" {
   api_management_name   = azurerm_api_management.apim.name
   resource_group_name   = azurerm_resource_group.rg.name
-  product_id            = azurerm_api_management_product.starter.display_name
-  user_id               = azurerm_api_management_user.js.user_id
+  product_id            = azurerm_api_management_product.starter.id
+  user_id               = azurerm_api_management_user.js.id
   display_name          = "Product Starter subscription"
 }
 
